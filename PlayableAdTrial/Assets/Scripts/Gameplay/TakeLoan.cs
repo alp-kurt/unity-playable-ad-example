b115a3bd8631f2119474f7f11dd78f4855936f4b
Alp Kurt <krtalp@gmail.com>
2025-02-11 09:58:37 +0100

    Refactor TakeLoan class
    Split its functionalities into two classes for seperation of responsibilities and adding a button confirmation at the narration.

--- PlayableAdTrial/Assets/Scripts/Gameplay/TakeLoan.cs
+++ PlayableAdTrial/Assets/Scripts/Gameplay/TakeLoan.cs
@@ -2,29 +2,18 @@ using UnityEngine;
 
 public class TakeLoan : MonoBehaviour
 {
-    [Header("Narrative Window")]
-    [SerializeField] private GameObject narrativeWindow;
-    [SerializeField] private GameObject loan;
+
     [SerializeField] private string notificationText;
+    [SerializeField] private GameObject loan;
 
-
-    private bool hasTriggered = false; // Ensure it triggers only once
-
-    private void OnTriggerEnter(Collider other)
+    public void TakeOutLoan()
     {
-        if (other.CompareTag("Player") && !hasTriggered)
-        {
-            hasTriggered = true; // Prevent multiple activations
+        Luna.Unity.Analytics.LogEvent($"Loan Taken", 1);
+        MoneySystem.instance.AddMoney(1000000);
+        Notifier.instance.ShowNotification(notificationText, 1.5f);
 
-            narrativeWindow.SetActive(true); // Turn on the narrative window
-            loan.SetActive(false); //Turn off the money object
+        loan.SetActive(false); //Turn off the money object
 
-            Luna.Unity.Analytics.LogEvent($"Loan Taken", 1);
-
-            MoneySystem.instance.AddMoney(1000000);
-            Notifier.instance.ShowNotification(notificationText, 1.5f);
-
-            gameObject.SetActive(false);
-        }
+        gameObject.SetActive(false);
     }
 }
